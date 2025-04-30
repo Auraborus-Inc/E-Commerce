cfbd893ed18cc7bdfd3e476ce3c36ad4d44486a6
Abdulla-h-Ibrahim <ab.ibrahim2k4@gmail.com>
2025-04-30 21:28:59 +0500

    EC - 17 Full
    Create an endpoint to register users via email/username & password
    
    Create an endpoint to login users

--- app.js
+++ app.js
@@ -7,24 +7,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get('/Home', async (req, res) => {
+app.get('/Home', (req, res) => {
     try {
-        const products = await database.getProducts();
-        console.log(products);
-        return res.send(`Welcome to Home Page Mr. ${req.query.name}. Products: ${JSON.stringify(products)}`);
+        return res.send(`Welcome to Home Page of the E Commerence Website ... `);
     } catch (err) {
         console.error(err);
         return res.status(500).send('Internal Server Error');
     }
 });
 
-app.post('/submit', (req, res) => {
-    return res.send(`Hello ${req.body.name} ... 
-      Welcome to the Page of Node JS using Express Server
-      you are ${req.body.age} years old`)
-  });
+app.get('/all_users', async (req, res) => {
+    try {
+        const users = await database('user').select('*');
+        return res.status(200).json(users);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error');
+    }
+});
 
-  app.post('/register', async (req, res) =>{
+app.post('/register', async (req, res) =>{
     const {name, email, password, phone_no, address} = req.body;
     if(!name || !email || !password){
         return res.status(400).send('Please fill all the fields')
@@ -49,7 +51,28 @@ app.post('/submit', (req, res) => {
         console.error(err);
         return res.status(500).send('Internal Server Error')
     }
-  })
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).send('Please fill all the fields')
+    }
+
+    try {
+        const user = await database('user')
+            .where({ email: email })
+            .andWhere({ password: password })
+            .first()
+        if (!user) {
+            return res.status(400).send('Invalid credentials')
+        }
+        return res.status(200).send('User logged in successfully')
+    } catch (err) {
+        console.error(err);
+        return res.status(500).send('Internal Server Error')
+    }
+})
 
 app.listen(port, () => {
     console.log("Server Started at http://localhost:8000")
