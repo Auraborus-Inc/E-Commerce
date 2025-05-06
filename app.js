29355364d110607609356213fe6f209605194fe5
Abdulla-h-Ibrahim <ab.ibrahim2k4@gmail.com>
2025-05-06 22:36:29 +0500

    Login Session Simple ... EC-22

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const session = require('express-session');
 const port = 8000;
 const database = require('./databseConnectivity.js');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60* 60 }
+}));
 
 
 app.get('/Home', (req, res) => {
@@ -67,10 +75,33 @@ app.post('/login', async (req, res) => {
         if (!user) {
             return res.status(400).send('Invalid credentials')
         }
+
+        req.session.user = {id: user.id, email: user.email}
         return res.status(200).send('User logged in successfully')
     } catch (err) {
         console.error(err);
         return res.status(500).send('Internal Server Error')
+    }
+})
+
+app.get('/dashboard', async (req, res) => {
+    if(req.session.user){
+        return res.status(200).send(`Welcome to the Dashboard`)
+    }
+    return res.status(401).send('First Login Please')
+})
+
+app.get('/logout', async (req, res) => {
+    if(req.session.user){
+        req.session.destroy((err) => {
+            if(err){
+                console.error(err);
+                return res.status(500).send('Internal Server Error')
+            }
+            return res.status(200).send('User logged out successfully')
+        })
+    }else{
+        return res.status(401).send('First Login Please')
     }
 })
 
