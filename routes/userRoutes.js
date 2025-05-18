0c56ed844d4cf4847b06b32455df8dc42f428256
Abdullah Ibrahim <ab.ibrahim2k4@gmail.com>
2025-05-18 15:29:41 +0500

    Revert "Ec 38"

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// userController.authTaoken,
 router.get('/Home', userController.homePage);
 router.get('/all_users', userController.getAllUsers);
 router.post('/register', userController.registerUser);
-router.post('/login',  userController.loginUser);
-router.get('/dashboard', userController.authToken, userController.dashboard);
+router.post('/login', userController.loginUser);
+router.get('/dashboard', userController.dashboard);
 router.get('/logout', userController.logoutUser);
 
-module.exports = router
+module.exports = router;
