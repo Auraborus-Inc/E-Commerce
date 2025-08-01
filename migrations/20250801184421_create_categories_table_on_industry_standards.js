338e6afb643eb8611ae5f7e2f8bcedb30addcafb
Abdulla-h-Ibrahim <ab.ibrahim2k4@gmail.com>
2025-08-02 00:03:31 +0500

    EC-48 BE: Create a categories table as per industry standards

---
+++ migrations/20250801184421_create_categories_table_on_industry_standards.js
@@ -0,0 +1,26 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('catagories', function(table){
+    table.increments('id').primary().notNullable().unique();
+    table.string('name').notNullable().unique();
+    table.text('description').nullable();
+
+    // Adding insdustry standards related fields
+    table.boolean('is_active').defaultTo(true);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+    table.timestamp('deleted_at').nullable();
+  });
+  
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('catagories');
+};
